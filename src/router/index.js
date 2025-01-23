1ddbeb886b4223bb6a27be32393ac7a5bf34fce9
yuxs <yuxs2022@163.com>
2025-01-23 16:24:44 +0800

    添加403/404错误页面及路由，优化用户中心视图

--- src/router/index.js
+++ src/router/index.js
@@ -19,16 +19,30 @@ const routes = [
   {
     path:'/about',
     name:'About',
-    component: () => import('@/components/updataPage/updata.vue')
+    component: () => import('@/components/updataPage/updata.vue'),
+  },
+  {
+    path:'/login',
+    name:'Login',
+    component: () => import('@/components/Login/login.vue'),
+    
+  },
+  {
+    path:'/add',
+    name:'AddResouce',
+    component: ()=>import('@/components/AddResouce/index.vue')
+  },
+  {
+    path:'/user',
+    name:'User',
+    component:()=>import('@/views/index.vue')
+  },
+   // 添加 404 错误页面路由
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'Err404',
+    component: ()=>import('@/components/Err404/index.vue') 
   }
-  // {
-  //   path: '/about',
-  //   name: 'about',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  // }
 ]
 
 const router = createRouter({
